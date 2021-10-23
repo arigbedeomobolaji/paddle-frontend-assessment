afcda0e0138c78c2cd7dbadc9d2cfc0f301a5f1b
Arigbede Omobolaji <arigbedeomobolaji@gmail.com>
2021-10-23 08:21:36 +0100

    edit header.jsx

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,11 +5,9 @@ module.exports = {
 		es2021: true,
 	},
 	extends: ['eslint:recommended', 'plugin:react/recommended'],
-	parser: 'babel-eslint',
 	parserOptions: {
 		ecmaFeatures: {
 			jsx: true,
-			experimentalObjectRestSpread: true,
 		},
 		ecmaVersion: 2020,
 		sourceType: 'module',
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,5 @@
 // jshint ignore:start
-import React from 'react';
+import React, { useState } from 'react';
 import styled, { withTheme } from 'styled-components';
 import Button from '../helpers/Button';
 import { Logo } from '../../style';
@@ -12,24 +12,66 @@ const Navbar = styled(ItemWrapper)``;
 
 const RightNavbar = styled(ItemWrapper)``;
 
+const Hamburger = styled.h1`
+	font-size: 3rem;
+`;
+const ResponsiveNavbarWrapper = styled.div`
+	position: absolute;
+	max-width: 400px;
+	right: 0;
+	background: #fff;
+	display: none;
+
+	@media (max-width: ${({ theme }) => theme.screen.tablet}) {
+	}
+`;
+
+const ResponsiveNavbar = styled(ItemWrapper)`
+	width: 100%;
+	flex-direction: column;
+	padding: 1rem;
+`;
+
 const Header = ({ theme }) => {
+	const [isClosed, setIsClosed] = useState(true);
 	return (
 		<>
 			<PageWrapper justifyContent='space-around' flexWrap>
 				<Logo>PEDDLE</Logo>
-				<Navbar justifyContent='flex-start' alignItems='center'>
-					<Link to='#'>About</Link>
-					<Link to='#'>FAQS</Link>
-					<Link to='#'>Pricing</Link>
-					<Link to='#'>Testimonials</Link>
-				</Navbar>
-				<RightNavbar justifyContent='flex-start' alignItems='center'>
-					<Link to='#' bold color={theme.color.dark}>
-						Signin
-					</Link>
-					<Button>Sign Up</Button>
-				</RightNavbar>
+				{isClosed && (
+					<>
+						<Navbar justifyContent='flex-start' alignItems='center'>
+							<Link to='#'>About</Link>
+							<Link to='#'>FAQS</Link>
+							<Link to='#'>Pricing</Link>
+							<Link to='#'>Testimonials</Link>
+						</Navbar>
+						<RightNavbar justifyContent='flex-start' alignItems='center'>
+							<Link to='#' bold color={theme.color.dark}>
+								Signin
+							</Link>
+							<Button>Sign Up</Button>
+						</RightNavbar>
+					</>
+				)}
+
+				<Hamburger onClick={setIsClosed(!isClosed)}>=</Hamburger>
 			</PageWrapper>
+
+			{!isClosed && (
+				<ResponsiveNavbarWrapper>
+					<ResponsiveNavbar>
+						<Link to='#'>About</Link>
+						<Link to='#'>FAQS</Link>
+						<Link to='#'>Pricing</Link>
+						<Link to='#'>Testimonials</Link>
+						<Link to='#' bold color={theme.color.dark}>
+							Signin
+						</Link>
+						<Button>Sign Up</Button>
+					</ResponsiveNavbar>
+				</ResponsiveNavbarWrapper>
+			)}
 		</>
 	);
 };
